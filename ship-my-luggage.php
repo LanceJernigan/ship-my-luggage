829ed8765fdb16b984bed00456bf790c4e65645c
LanceJernigan <lance@knoxweb.com>
2016-12-21 12:31:18 -0500

    Initial Commit

---
+++ ship-my-luggage.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*
+     *  Plugin Name:  Ship My Luggage
+     *  Description:  Functionality for Ship My Luggage
+     *  Version:      0.0.1
+     *  Author:       Lance Jernigan
+     *  Author URI:   http://www.LanceJernigan.com
+     */
+
+?>
